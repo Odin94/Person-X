98ce2cf9dd6ae84aeba828e6e44bd8669a3cd116
Andreas Odin Kammerloher <kammerloher.andreas@gmail.com>
2021-01-02 22:25:40 +0100

    Add sample fetch / store operations

---
+++ src/storage.js
@@ -0,0 +1,29 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+firebase.initializeApp({
+    apiKey: '### FIREBASE API KEY ###',
+    authDomain: '### FIREBASE AUTH DOMAIN ###',
+    projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+});
+
+var db = firebase.firestore();
+
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+    .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+        console.error("Error adding document: ", error);
+    });
+
+db.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
